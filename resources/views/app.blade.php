fd047bc1fec8335d4c55b13e1a764ecee7dc2b7a
tig <tig@example.com>
2017-06-25 12:21:21 +0000

    Undo remove bootstrap-theme.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,11 +25,9 @@
 
         {{-- bootstrap --}}
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
-        {{-- bootstrap-theme --}}
         <link href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether-theme-arrows-dark.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
 
         {{-- recaptcha --}}
         <script src='https://www.google.com/recaptcha/api.js'></script>
@@ -41,6 +39,9 @@
         <script>hljs.initHighlightingOnLoad();</script>
 
         <script src="{{asset('js/app.js')}}"></script>
+
+        {{-- bootstrap-theme --}}
+        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
         @yield('head')
     </head>
